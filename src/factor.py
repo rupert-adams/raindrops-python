dd4b30b0ed1cb1f5bb55e7b3a188d6b6d593faf7
Rupert Adams <RupertNCAdams@aol.com>
2019-02-18 20:27:51 +0000

    deleted useless factor finder method

--- src/factor.py
+++ src/factor.py
@@ -8,13 +8,3 @@ class Factor(object):
 
         list = [s for f, s in drops if self.number % f == 0]
 	return "".join(list) if list else str(self.number)
-
-    @property
-    def getFactor(self):
-        factors=[];
-
-        for i in range(1, self.number + 1):
-            if self.number % i == 0:
-                factors.append(i)
-
-        return factors
