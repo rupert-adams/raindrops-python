f92751ebf6b80ccf8cbdb4d853dacfad4e5e4193
Rupert Adams <RupertNCAdams@aol.com>
2019-02-18 18:59:25 +0000

    added test for finding factors of given number

--- tests/factor_test.py
+++ tests/factor_test.py
@@ -5,3 +5,6 @@ class FactorTest(unittest.TestCase):
 
     def test_can_print_number_as_string(self):
         assert Factor(4).numb == "4"
+
+    def test_can_get_factors_of_number(self):
+        assert Factor(34).getFactor == [1, 2, 17, 34]
