7f5731e6cd02b90bfa81b6c9aacd444f92e956ac
Rupert Adams <RupertNCAdams@aol.com>
2019-02-18 17:27:38 +0000

    changed test to return string instead of int

--- tests/factor_test.py
+++ tests/factor_test.py
@@ -4,4 +4,4 @@ from factor import Factor
 class FactorTest(unittest.TestCase):
 
     def test_can_print_number_as_string(self):
-        assert Factor(4).numb == 4
+        assert Factor(4).numb == "4"
