008139ba2143b1a13560a5da1670db91f8ae4f02
Rupert Adams <RupertNCAdams@aol.com>
2019-02-18 19:49:40 +0000

    added first test towards criteria

--- tests/factor_test.py
+++ tests/factor_test.py
@@ -8,3 +8,6 @@ class FactorTest(unittest.TestCase):
 
     def test_can_get_factors_of_number(self):
         assert Factor(34).getFactor == [1, 2, 17, 34]
+
+    def test_can_return_pling_if_number_divisible_by_3(self):
+        assert Factor(3).numb == "pling"
